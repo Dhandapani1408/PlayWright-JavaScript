d5b1fc4e5ebe4dc6d5b2063e77c7115bcab31dfa
Dhandapani1408 <dhandapani1408@gmail.com>
2024-07-21 22:16:05 +0530

    learning string

---
+++ tests-examples/day02/03_looping_statements.js
@@ -0,0 +1,23 @@
+/**
+ * 1. For loop => for(initilization , condition, increment/decrement )
+ * 2. While loop
+ * 3. do while loop
+ * 
+ */
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+
+}
+console.log("---------------------------------------");
+let count = -1
+while (count > 0) {
+    console.log(count);
+    count--;
+}
+console.log("=================================");
+let counting = -1
+do {
+    console.log(counting);
+    counting--;
+} while (counting > 0);
